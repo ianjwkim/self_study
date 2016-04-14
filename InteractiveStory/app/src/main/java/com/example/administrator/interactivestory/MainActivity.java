60170067a9073b34e9b59c0013c83caed744bec7
ianjwkim <ian.kimjw@gmail.com>
2016-04-14 19:52:02 +0800

    First Commit

---
+++ InteractiveStory/app/src/main/java/com/example/administrator/interactivestory/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.administrator.interactivestory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameField;
+    private Button mStartButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameField = (EditText) findViewById(R.id.nameEditText);
+        mStartButton = (Button) findViewById(R.id.button);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name =  mNameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    private void startStory(String name){
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
+    }
+}
